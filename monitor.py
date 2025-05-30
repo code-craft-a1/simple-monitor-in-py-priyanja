001a5f0fe2eab33d8afbf85532acbdeb0d049f6a
priyanja <priyanja@adobe.com>
2025-05-30 04:56:16 +0000

    Modular and low complexity code

--- monitor.py
+++ monitor.py
@@ -2,36 +2,35 @@
 from time import sleep
 import sys
 
+def is_temperature_ok(temperature):
+  return 95 <= temperature <= 102
+
+def alert_message(message):
+  print(message)
+  for i in range(6):
+    print('\r* ', end='')
+    sys.stdout.flush()
+    sleep(1)
+    print('\r *', end='')
+    sys.stdout.flush()
+    sleep(1)
+
+def is_pulse_ok(pulse):
+  return 60 <= pulse <= 100
+
+def is_spo2_ok(spo2):
+  return spo2>=90
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+  are_vitals_ok=True
+  if not is_temperature_ok(temperature):
+    alert_message('Temperature critical!')
+    are_vitals_ok= False
+  if not is_pulse_ok(pulseRate):
+    alert_message('Pulse Rate is out of range!')
+    are_vitals_ok= False
+  if not is_spo2_ok(spo2):
+    alert_message('Oxygen Saturation out of range!')
+    are_vitals_ok= False
+  return are_vitals_ok
+
--- monitor.test.py
+++ monitor.test.py
@@ -1,11 +1,32 @@
 import unittest
-from monitor import vitals_ok
+from monitor import *
 
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
-        self.assertTrue(vitals_ok(98.1, 70, 98))
+  def test_temperature(self):
+    self.assertTrue(is_temperature_ok(98.6))
+    self.assertFalse(is_temperature_ok(103))
+    self.assertFalse(is_temperature_ok(94))
+  
+  def test_pulse(self):
+    self.assertTrue(is_pulse_ok(70))
+    self.assertFalse(is_pulse_ok(59))
+    self.assertFalse(is_pulse_ok(101))
+
+  def test_spo2(self):
+    self.assertTrue(is_spo2_ok(95))
+    self.assertFalse(is_spo2_ok(89))
+
+  def test_vitals_ok(self):
+    self.assertTrue(vitals_ok(98.1, 70, 98))
+    
+  
+  def test_not_ok_when_any_vital_out_of_range(self):
+    self.assertFalse(vitals_ok(99, 102, 70))
+    self.assertFalse(vitals_ok(103, 70, 95))
+    self.assertFalse(vitals_ok(98.6, 101, 95))
+    self.assertFalse(vitals_ok(103, 101, 89))
+    self.assertFalse(vitals_ok(99, 102, 70))
 
 
 if __name__ == '__main__':
